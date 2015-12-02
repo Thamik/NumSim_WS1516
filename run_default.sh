eab97092eba9586d3ff2897764461aec0fa64f18
BastiSuttgart <st100650@stud.uni-stuttgart.de>
2015-12-02 14:36:07 +0100

    run_default modified again

--- run_default.sh
+++ run_default.sh
@@ -1,5 +1,5 @@
 for numproc in 1 2 4 9 16
 do
 	echo $numproc
-	mpirun -np $numproc numsim data/my_default.params data/default.geom
+	mpirun -np $numproc numsim data/my_default.params data/medium_grid.geom
 done
