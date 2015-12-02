fa98a4469e76abc749ca8dcfa9015b08aa2b2074
BastiSuttgart <st100650@stud.uni-stuttgart.de>
2015-12-02 14:33:00 +0100

    run_default modified

--- run_default.sh
+++ run_default.sh
@@ -1,1 +1,5 @@
-mpirun -np 4 numsim data/my_default.params data/default.geom
+for numproc in 1 2 4 9 16
+do
+	echo $numproc
+	mpirun -np $numproc numsim data/my_default.params data/default.geom
+done
