e10f1f778e241c7f91b27b4aa6acb085c7f80062
dominik.leichtle <dominik.leichtle@web.de>
2016-01-04 19:45:16 +0100

    something

--- uq_sampling/do_statistics_adv.py
+++ uq_sampling/do_statistics_adv.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         stats = Statistics()
         stats.loadSimData('VTK')
         stats.setTimes(np.linspace(0,30,100))
-	stats.setGrid(np.linspace(1000,2000,50))
+	stats.setGrid(np.linspace(1000,2000,200))
         stats.compute()
 	stats.computeTrap()
 	stats.computeConvergence()
