4d651ce1a3ea5b247010ad77f80dee9aee163050
dominik.leichtle <dominik.leichtle@web.de>
2016-01-11 13:43:02 +0100

    new data files

--- uq_sampling/run_samples.py
+++
@@ -1,7 +0,0 @@
-from uq_sampling import SimFrame
-
-if __name__ == '__main__':
-        # only run when executed as a script
-        frame = SimFrame(500, 4)
-        frame.generateSamples()
-        frame.run()
